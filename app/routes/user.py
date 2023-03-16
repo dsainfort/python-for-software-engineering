65cda137f2ff79c721c160c9df65d8b6918b2e06
Djifferson Sainfort <43509798+dsainfort@users.noreply.github.com>
2023-03-16 03:11:07 -0400

    refactoring

--- app/routes/user.py
+++ app/routes/user.py
@@ -6,48 +6,52 @@ from app.schemas.User import (
 )
 from app.services.user import UserService
 
-user_router = APIRouter()
-user_service = UserService()
+
+def create_user_router() -> APIRouter:
+    user_router = APIRouter()
+    user_service = UserService()
 
 
-@user_router.get("/user/me", response_model=Profile)
-async def get_user():
-    user_profile = await user_service.get_user_info()
+    @user_router.get("/user/me", response_model=Profile)
+    async def get_user():
+        user_profile = await user_service.get_user_info()
 
-    return user_profile
-
-
-@user_router.get("/user/{user_id}", response_model=Profile)
-async def get_user_by__id(user_id: int):
-    """
-    Endpoint for retrieving a full user profile byt the user's unique ID.
-
-    :param user_id: int - unique monotonically increasing integer id
-    :return: FullUserProfile
-    """
-    user_profile = await user_service.get_user_info(user_id)
-    return user_profile
+        return user_profile
 
 
-@user_router.put("/users/{user_id}")
-async def update_user(user_id: int, full_user_info: Profile):
-    await user_service.create_update_user(full_user_info, user_id)
-    return None
+    @user_router.get("/user/{user_id}", response_model=Profile)
+    async def get_user_by__id(user_id: int):
+        """
+        Endpoint for retrieving a full user profile byt the user's unique ID.
+
+        :param user_id: int - unique monotonically increasing integer id
+        :return: FullUserProfile
+        """
+        user_profile = await user_service.get_user_info(user_id)
+        return user_profile
 
 
-@user_router.delete("/users/{user_id}")
-async def remove_user(user_id: int):
-    await user_service.delete_user(user_id)
+    @user_router.put("/users/{user_id}")
+    async def update_user(user_id: int, full_user_info: Profile):
+        await user_service.create_update_user(full_user_info, user_id)
+        return None
 
 
-@user_router.get("/users", response_model=ManyUserResponse)
-async def get_all_users_paginated(start: int = 0, limit: int = 2):
-    users, total = await user_service.get_all_users_with_pagination(start, limit)
-    formatted_users = ManyUserResponse(users=users, total=total)
-    return formatted_users
+    @user_router.delete("/users/{user_id}")
+    async def remove_user(user_id: int):
+        await user_service.delete_user(user_id)
 
 
-@user_router.post("/users", response_model=CreateUserRes)
-async def add_user(full_user_info: Profile):
-    user_id = await user_service.create_update_user(full_user_info)
-    return CreateUserRes(user_id=user_id)
+    @user_router.get("/users", response_model=ManyUserResponse)
+    async def get_all_users_paginated(start: int = 0, limit: int = 2):
+        users, total = await user_service.get_all_users_with_pagination(start, limit)
+        formatted_users = ManyUserResponse(users=users, total=total)
+        return formatted_users
+
+
+    @user_router.post("/users", response_model=CreateUserRes)
+    async def add_user(full_user_info: Profile):
+        user_id = await user_service.create_update_user(full_user_info)
+        return CreateUserRes(user_id=user_id)
+
+    return user_router
