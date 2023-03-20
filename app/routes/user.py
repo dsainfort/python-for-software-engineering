c6da1d694b2f0c4f6a60af2c8e4188c418af9ad4
Djifferson Sainfort <43509798+dsainfort@users.noreply.github.com>
2023-03-20 16:39:03 -0400

    env variables

--- app/routes/user.py
+++ app/routes/user.py
@@ -8,18 +8,19 @@ from app.services.user import UserService
 
 
 def create_user_router() -> APIRouter:
-    user_router = APIRouter()
+    user_router = APIRouter(
+        prefix="/user",
+        tags=["User"]
+    )
     user_service = UserService()
 
+    @user_router.get("/all", response_model=ManyUserResponse)
+    async def get_all_users_paginated(start: int = 0, limit: int = 2):
+        users, total = await user_service.get_all_users_with_pagination(start, limit)
+        formatted_users = ManyUserResponse(users=users, total=total)
+        return formatted_users
 
-    @user_router.get("/user/me", response_model=Profile)
-    async def get_user():
-        user_profile = await user_service.get_user_info()
-
-        return user_profile
-
-
-    @user_router.get("/user/{user_id}", response_model=Profile)
+    @user_router.get("/{user_id}", response_model=Profile)
     async def get_user_by__id(user_id: int):
         """
         Endpoint for retrieving a full user profile byt the user's unique ID.
@@ -31,25 +32,17 @@ def create_user_router() -> APIRouter:
         return user_profile
 
 
-    @user_router.put("/users/{user_id}")
+    @user_router.put("/{user_id}")
     async def update_user(user_id: int, full_user_info: Profile):
         await user_service.create_update_user(full_user_info, user_id)
         return None
 
 
-    @user_router.delete("/users/{user_id}")
+    @user_router.delete("/{user_id}")
     async def remove_user(user_id: int):
         await user_service.delete_user(user_id)
 
-
-    @user_router.get("/users", response_model=ManyUserResponse)
-    async def get_all_users_paginated(start: int = 0, limit: int = 2):
-        users, total = await user_service.get_all_users_with_pagination(start, limit)
-        formatted_users = ManyUserResponse(users=users, total=total)
-        return formatted_users
-
-
-    @user_router.post("/users", response_model=CreateUserRes)
+    @user_router.post("/", response_model=CreateUserRes)
     async def add_user(full_user_info: Profile):
         user_id = await user_service.create_update_user(full_user_info)
         return CreateUserRes(user_id=user_id)
--- main.py
+++ main.py
@@ -3,9 +3,9 @@ from app.routes.user import create_user_router
 
 
 def create_application() -> FastAPI:
-  user_router = create_user_router()
-  app = FastAPI()
-  app.include_router(user_router)
-  return app
+    user_router = create_user_router()
+    app = FastAPI()
+    app.include_router(user_router)
+    return app
 
 app = create_application()
